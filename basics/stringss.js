c98508d22df6f49b3642adcc4a30853990458054
Abhi Varma <111688945+abhivarma174@users.noreply.github.com>
2023-11-27 20:53:14 +0000

    stringss

---
+++ basics/stringss.js
@@ -0,0 +1,24 @@
+const name="abhi";
+const Count=50;
+//console.log(name+Count+" Value");
+//Above is outdated method
+/*console.log(`my name is ${name} and my count is ${Count}`);*/
+const myName=new String("AbhiVarma");
+//console.log(myName);
+//console.log(myName[0]);
+//console.log(myName.toUpperCase());
+//console.log(myName.length);
+//console.log(myName.charAt(5));
+//console.log(myName.indexOf('m'));
+//const newString=myName.substring(0,4);
+//console.log(newString);
+//const AnotherString=myName.slice(-7,4);
+//console.log(AnotherString);
+/*const Str=new String("   Abhixx   ");
+console.log(Str.trim());
+console.log(Str.replace('x','n'));
+console.log(Str.replace('x','n'));*/
+const mystr=new String("My name is Abhi Varma");
+//console.log(mystr);
+console.log(mystr.split(' '));
+//console.log(mystr.split(''));
