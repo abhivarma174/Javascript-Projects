5f65dd621948b03a52624577b72aa4d67beab844
Abhi Varma <111688945+abhivarma174@users.noreply.github.com>
2023-12-01 23:11:01 +0000

    date and time

---
+++ basics/date_time.js
@@ -0,0 +1,16 @@
+let myDate=new Date();
+//console.log(typeof myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+
+//let CreatedDate=new Date(2023,0,23);
+//let CreatedDate=new Date(2023,0,23,5,3);
+//console.log(CreatedDate);
+
+//let myDatee=new Date("13-12-2023");
+//console.log(myDatee);
+//let newDate=new Date("12-02-2023");
+//console.log(newDate);
+
+let myTimestamp=Date.now();
+console.log(myTimestamp);
